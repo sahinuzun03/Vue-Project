2d1691752bdb21b04bdb00c3bb8ec3e0c57f511e
Sahin Uzun <sahinuzun03@gmail.com>
2023-02-01 17:06:29 +0300

    Sayfa tasarımları ayarlanmıştır

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+  <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+      <h2>{{ project.title }}</h2>
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  data(){
+    return{
+      projects:[]
+    }
+  },
+  mounted(){
+    //Proje ilk yüklendiği zaman çalışacak olan metot
+    fetch('http://localhost:3008/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
+  }
+}
+</script>
